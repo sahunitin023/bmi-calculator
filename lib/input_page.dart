e69866c5d2bfd89089610e8c890dd6e0f6f5aa07
sahunitin023 <b521039@iiit-bh.ac.in>
2022-10-13 03:28:58 +0530

    Designed the Card for Input

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,7 @@ class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _InputPageState createState() => _InputPageState();
 }
 
@@ -14,8 +15,61 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFf1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFf1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(
+              colour: Color(0xFf1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFf1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFf1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  const ReusableCard({Key? key, required this.colour}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
       ),
     );
   }
